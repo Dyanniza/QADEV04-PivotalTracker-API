9d5184472fe20010787efe365241a846e6d2620a
j4z31 <tumael_777@hotmail.com>
2015-10-20 18:10:53 -0400

    Refactoring the Test Case of Story Service

--- test/stories.js
+++ test/stories.js
@@ -1,34 +1,67 @@
-// STORIE.JS
+/*
+@author Jhasmany Quiroz
+@class STORIES for Test Cases the Services of 'Stories'.
+ */
 
 var expect =  require('chai').expect;
 var request = require('superagent');
 require('superagent-proxy')(request);
 
+var config = require('..//config');
+var tokenAPI = require('../lib/tokenAPI');
+var storiesApi = require('../lib/storiesApi');
+
+
+/**
+ * @param  Test to Service's Stories
+ * @return Passing or Failing of Test Case executed.
+ */
 describe('STORY of Project', function(){
 	this.timeout(10000);
-	it.only('GET /projects/{project_id}/stories', function (done) {
-		request
-			.get('https://www.pivotaltracker.com/services/v5/projects/1448488/stories/')
-			//.proxy('http://172.20.240.5:8080')
-			.set('X-TrackerToken', '15e4bbf7b3228e3d00b28418d930f68a')
-		.end(function(err, res){
-			console.log(res.body);
-			expect(res.status).to.equal(200);
-			done();
-		});
+
+	var userCredential = config.userCredential;
+
+	var token = null;
+	var projectId = null;
+
+	before('Getting the token', function(done){
+		tokenAPI
+			.getToken(userCredential, function(res){				
+				token = res.body;
+				console.log(token);
+				expect(token.username).to.equal(userCredential.userAccount);
+				done();
+			});
 	});
 
-	it('POST /projects/{project_id}/stories', function (done) {
-		request
-			.post('https://www.pivotaltracker.com/services/v5/projects/1448498/stories')
-			//.proxy('http://172.20.240.5:8080')
-			.set('X-TrackerToken', '15e4bbf7b3228e3d00b28418d930f68a')
-			.set('Content-Type', 'application/json')
-			.send({'name':'User Story #2 of Api Testing v1.0'})
-		.end(function(err, res){
-			console.log(res.body);
-			expect(res.status).to.equal(200);
-			done();
-		});
-	});
-});
+	/*after('Deleting the token', function(done){
+		tokenAPI
+			.deleteToken(tk, function(res){	
+				var tkDeleted = res.body;
+				expect(tkDeleted.api_token).to.equal(tk.api_token);
+				tk = null;
+				done();
+			});
+	});*/
+
+	it('GET /projects/{project_id}/stories', function (done) {
+		projectId = -1;
+		storiesApi
+			.getStories(projectId, token.api_token, function(res){				
+				expect(res).to.equal(404);
+
+				done();
+			});
+	});//it
+
+	it.only('POST /projects/{project_id}/stories', function (done) {
+		projectId = -1;
+		storiesApi
+			.postStories(projectId, token.api_token, function(res){
+				expect(res.code).to.equal(config.codeStories);
+				console.log('RESPONSE: ', res);
+
+				done();
+			});
+	});//it
+});//describe
--- test/story.js
+++ test/story.js
@@ -1,50 +1,78 @@
-// STORY TESTS
+/*
+@author Jhasmany Quiroz
+@class STORIES for Test Cases the Services of 'Stories'.
+ */
+
 var expect =  require('chai').expect;
 var request = require('superagent');
 require('superagent-proxy')(request);
 
-describe('STORY of Project', function(){
-	//this.timeout(10000);
+var config = require('..//config');
+var tokenAPI = require('../lib/tokenAPI');
+var storyApi = require('../lib/storyApi');
 
-	it('GET /projects/{project_id}/stories/{story_id}', function (done) {
-		request
-			.get('https://www.pivotaltracker.com/services/v5/projects/1448488/stories/105576868')
-			//.proxy('http://172.20.240.5:8080')
-			.set('X-TrackerToken', '15e4bbf7b3228e3d00b28418d930f68a')
-		.end(function(err, res){
-			console.log(res.body);
-			expect(res.status).to.equal(200);
-			done();
-		});
+
+/**
+ * @param  Test to Service's Story
+ * @return Passing or Failing of Test Case executed.
+ */
+describe('STORY of Project', function(){
+	this.timeout(10000);
+
+	var userCredential = config.userCredential;
+
+	var token = null;
+	var projectId = null;
+
+	before('Getting the token', function(done){
+		tokenAPI
+			.getToken(userCredential, function(res){				
+				token = res.body;
+				console.log(token);
+				expect(token.username).to.equal(userCredential.userAccount);
+				done();
+			});
+	});
+
+	/*after('Deleting the token', function(done){
+		tokenAPI
+			.deleteToken(tk, function(res){	
+				var tkDeleted = res.body;
+				expect(tkDeleted.api_token).to.equal(tk.api_token);
+				tk = null;
+				done();
+			});
+	});*/
+
+	it.only('GET /projects/{project_id}/stories/{story_id}', function (done) {
+		projectId = storyId = -1;
+		storyApi
+			.getStory(projectId, storyId, token.api_token, function(res){
+				expect(res).to.equal(404);
+
+				done();
+			});
 	});//it
 
 	it('PUT /projects/{project_id}/stories/{story_id}', function (done) {
-		request
-			.put('https://www.pivotaltracker.com/services/v5/projects/1448488/stories/105576868')
-			//.proxy('http://172.20.240.5:8080')
-			.set('X-TrackerToken', '15e4bbf7b3228e3d00b28418d930f68a')
-			.set('Content-Type', 'application/json')
-			.send({'name':'User Story #2 modified by put test'})
-		.end(function(err, res){
-			console.log(res.body);
-			expect(res.status).to.equal(400);
-			done();
-		});
+		projectId = storyId = -1;
+		storyApi
+			.putStory(projectId, storyId, token.api_token, function(res){
+				expect(res.code).to.equal(config.codeStory);
+				console.log('RESPONSE: ', res);
+
+				done();
+			});
 	});//it
 
 	it.only('DELETED /projects/{project_id}/stories/{story_id}', function (done) {
-		request
-			.put('https://www.pivotaltracker.com/services/v5/projects/1448488/stories/105576868')
-			//.proxy('http://172.20.240.5:8080')
-			.set('X-TrackerToken', '15e4bbf7b3228e3d00b28418d930f68a')
-			.set('Content-Type', 'application/json')
-			.send({'name':'User Story #2 modified by put test'})
-		.end(function(err, res){
-			console.log(res.body);
-			expect(res.status).to.equal(400);
-			done();
-		});
-	});//it
+		projectId = storyId = -1;
+		storyApi
+			.deleteStory(projectId, storyId, token.api_token, function(res){
+				expect(res.code).to.equal(config.codeStory);
+				console.log('RESPONSE: ', res);
 
-	
+				done();
+			});
+	});//it	
 });//describe
