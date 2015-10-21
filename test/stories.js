5f040ed0bab53e28c76697718477c40ade8f20f9
j4z31 <tumael_777@hotmail.com>
2015-10-20 22:04:11 -0400

    Refactoring the Stories and Story Services

--- test/stories.js
+++ test/stories.js
@@ -7,7 +7,7 @@ var expect =  require('chai').expect;
 var request = require('superagent');
 require('superagent-proxy')(request);
 
-var config = require('..//config');
+var config = require('../config');
 var tokenAPI = require('../lib/tokenAPI');
 var storiesApi = require('../lib/storiesApi');
 
@@ -16,52 +16,36 @@ var storiesApi = require('../lib/storiesApi');
  * @param  Test to Service's Stories
  * @return Passing or Failing of Test Case executed.
  */
-describe('STORY of Project', function(){
-	this.timeout(10000);
-
+describe('STORY of Project', function() {
+	this.timeout(config.timeout);
 	var userCredential = config.userCredential;
-
 	var token = null;
 	var projectId = null;
 
-	before('Getting the token', function(done){
+	before('Getting the token', function(done) {
 		tokenAPI
-			.getToken(userCredential, function(res){				
-				token = res.body;
-				console.log(token);
+			.getToken(userCredential, function(res) {
+				token = res.body;				
 				expect(token.username).to.equal(userCredential.userAccount);
+				done();
+			});
+	});	
+
+	it('GET /projects/{project_id}/stories', function(done) {
+		projectId = -1;
+		storiesApi
+			.getStories(projectId, token.api_token, function(res) {
+				expect(res).to.equal(404);
 				done();
 			});
 	});
 
-	/*after('Deleting the token', function(done){
-		tokenAPI
-			.deleteToken(tk, function(res){	
-				var tkDeleted = res.body;
-				expect(tkDeleted.api_token).to.equal(tk.api_token);
-				tk = null;
-				done();
-			});
-	});*/
-
-	it('GET /projects/{project_id}/stories', function (done) {
+	it.only('POST /projects/{project_id}/stories', function(done) {
 		projectId = -1;
 		storiesApi
-			.getStories(projectId, token.api_token, function(res){				
-				expect(res).to.equal(404);
-
+			.postStories(projectId, token.api_token, function(res) {
+				expect(res.code).to.equal(config.codeStories);				
 				done();
 			});
-	});//it
-
-	it.only('POST /projects/{project_id}/stories', function (done) {
-		projectId = -1;
-		storiesApi
-			.postStories(projectId, token.api_token, function(res){
-				expect(res.code).to.equal(config.codeStories);
-				console.log('RESPONSE: ', res);
-
-				done();
-			});
-	});//it
-});//describe
+	});
+});
--- test/story.js
+++ test/story.js
@@ -17,10 +17,8 @@ var storyApi = require('../lib/storyApi');
  * @return Passing or Failing of Test Case executed.
  */
 describe('STORY of Project', function(){
-	this.timeout(10000);
-
+	this.timeout(config.timeout);
 	var userCredential = config.userCredential;
-
 	var token = null;
 	var projectId = null;
 
@@ -32,47 +30,32 @@ describe('STORY of Project', function(){
 				expect(token.username).to.equal(userCredential.userAccount);
 				done();
 			});
-	});
-
-	/*after('Deleting the token', function(done){
-		tokenAPI
-			.deleteToken(tk, function(res){	
-				var tkDeleted = res.body;
-				expect(tkDeleted.api_token).to.equal(tk.api_token);
-				tk = null;
-				done();
-			});
-	});*/
+	});	
 
 	it.only('GET /projects/{project_id}/stories/{story_id}', function (done) {
 		projectId = storyId = -1;
 		storyApi
 			.getStory(projectId, storyId, token.api_token, function(res){
 				expect(res).to.equal(404);
-
 				done();
 			});
-	});//it
+	});
 
 	it('PUT /projects/{project_id}/stories/{story_id}', function (done) {
 		projectId = storyId = -1;
 		storyApi
 			.putStory(projectId, storyId, token.api_token, function(res){
-				expect(res.code).to.equal(config.codeStory);
-				console.log('RESPONSE: ', res);
-
+				expect(res.code).to.equal(config.codeStory);				
 				done();
 			});
-	});//it
+	});
 
-	it.only('DELETED /projects/{project_id}/stories/{story_id}', function (done) {
+	it('DELETED /projects/{project_id}/stories/{story_id}', function (done) {
 		projectId = storyId = -1;
 		storyApi
 			.deleteStory(projectId, storyId, token.api_token, function(res){
-				expect(res.code).to.equal(config.codeStory);
-				console.log('RESPONSE: ', res);
-
+				expect(res.code).to.equal(config.codeStory);				
 				done();
 			});
-	});//it	
-});//describe
+	});
+});
