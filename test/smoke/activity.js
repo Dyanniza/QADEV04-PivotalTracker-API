d172bfaa82148de8e331dd7d141afe50270f1d29
j4z31 <tumael_777@hotmail.com>
2015-10-23 15:51:41 -0400

    Following the standars code with jsbeautifier tool

--- test/smoke/activity.js
+++ test/smoke/activity.js
@@ -1,8 +1,8 @@
 /*
 @author Jhasmany Quiroz
-@Class STORIES for Test Cases the Services of 'Stories'.
+@Class ACTIVITY for Test Cases the Services of 'Activity'.
  */
-var expect =  require('chai').expect;
+var expect = require('chai').expect;
 var request = require('superagent');
 require('superagent-proxy')(request);
 var Chance = require('chance');
@@ -16,92 +16,92 @@ var endPoints = require('../../resources/endPoints');
  * @param  Test to Service's Stories
  * @return Passing or Failing of Test Case executed.
  */
-describe('ACTIVITYS ABOUT THE PROJECT', function(){
-  this.timeout(config.timeout);
-  var userCredential = config.userCredential;
-  var token = null;
-  var project_id = null;
-  var chance = new Chance();
-  var story = {
-    name: chance.string(),
-    id: -1
-  };
+describe('ACTIVITYS ABOUT THE PROJECT', function() {
+    this.timeout(config.timeout);
+    var userCredential = config.userCredential;
+    var token = null;
+    var project_id = null;
+    var chance = new Chance();
+    var story = {
+        name: chance.string(),
+        id: -1
+    };
 
-  before('GETTING THE TOKEN AND CREATED THE FILES', function(done) {
-    tokenAPI
-      .getToken(userCredential, function(res) {
-        token = res.body;
-        expect(token.username).to.equal(userCredential.userAccount);
-        var newProject = {
-          name: chance.string()
-        };
+    before('GETTING THE TOKEN AND CREATED THE FILES', function(done) {
+        tokenAPI
+            .getToken(userCredential, function(res) {
+                token = res.body;
+                expect(token.username).to.equal(userCredential.userAccount);
+                var newProject = {
+                    name: chance.string()
+                };
 
-        var projectsEndPoint = endPoints.projects.projectsEndPoint;
+                var projectsEndPoint = endPoints.projects.projectsEndPoint;
+                generalLib
+                    .post(newProject, token.api_token, projectsEndPoint, function(res) {
+                        expect(res.status).to.equal(200);
+                        project_id = res.body.id;
+                        done();
+                    });
+            });
+    });
+
+    after('DELETED THE FILES', function(done) {
+        var endPoint = endPoints.projects.projectByIdEndPoint.replace('{project_id}', project_id);
         generalLib
-          .post(newProject, token.api_token, projectsEndPoint, function(res){
-            expect(res.status).to.equal(200);
-            project_id = res.body.id;
-            done();
-          });
-      });
-  });
+            .del(token.api_token, endPoint, function(res) {
+                expect(res.status).to.equal(204);
+                projectId = -1;
+                storyId = -1;
+                done();
+            });
+    });
 
-  after('DELETED THE FILES', function (done) {
-    var endPoint = endPoints.projects.projectByIdEndPoint.replace('{project_id}', project_id);
-      generalLib
-        .del(token.api_token, endPoint, function(res){
-          expect(res.status).to.equal(204);
-          projectId = -1;
-            storyId = -1;
-            done();
-        });
-  });
+    it('GET /my/activity', function(done) {
+        var myActivitysEndPoint = endPoints.activity.myActivitysEndPoint;
+        generalLib
+            .get(token.api_token, myActivitysEndPoint, function(res) {
+                expect(res.status).to.equal(200);
+                done();
+            });
+    });
 
-  it('GET /my/activity', function (done) {
-    var myActivitysEndPoint = endPoints.activity.myActivitysEndPoint;
-    generalLib
-      .get(token.api_token, myActivitysEndPoint, function(res){
-        expect(res.status).to.equal(200);
-        done();
-      });    
-  });
+    //return list of the selected project activity.
+    it('GET /projects/{project_id}/activity', function(done) {
+        var myActivitysProject = endPoints.activity.myActivitysProject.replace('{project_id}', project_id);
+        generalLib
+            .get(token.api_token, myActivitysProject, function(res) {
+                expect(res.status).to.equal(200);
+                done();
+            });
+    });
 
-  //return list of the selected project activity.
-  it('GET /projects/{project_id}/activity', function (done) {
-    var myActivitysProject = endPoints.activity.myActivitysProject.replace('{project_id}', project_id);
-    generalLib
-      .get(token.api_token, myActivitysProject, function(res){
-        expect(res.status).to.equal(200);
-        done();
-      });      
-  });
+    //return list of the selected story's activity.
+    it('GET /projects/{project_id}/stories/{story_id}/activity', function(done) {
+        var storiesEndPoint = endPoints.stories.storiesEndPoint.replace('{project_id}', project_id);
+        generalLib
+            .post(story, token.api_token, storiesEndPoint, function(res) {
+                expect(res.status).to.equal(200);
+                story.id = res.body.id;
 
-  //return list of the selected story's activity.
-  it('GET /projects/{project_id}/stories/{story_id}/activity', function (done) {
-    var storiesEndPoint = endPoints.stories.storiesEndPoint.replace('{project_id}', project_id);
-    generalLib
-      .post(story, token.api_token, storiesEndPoint, function(res){
-        expect(res.status).to.equal(200);
-        story.id = res.body.id;
-        
-        var myActivitysStorie =  endPoints.activity.myActivitysStorie.replace('{project_id}', project_id);
-        myActivitysStorie = myActivitysStorie.replace('{story_id}', story.id);
-          generalLib
-            .get(token.api_token, myActivitysStorie, function(res){
-              expect(res.status).to.equal(200);
-              done();
-            });          
-      });      
-  });
+                var myActivitysStorie = endPoints.activity.myActivitysStorie.replace('{project_id}', project_id);
+                myActivitysStorie = myActivitysStorie.replace('{story_id}', story.id);
+                generalLib
+                    .get(token.api_token, myActivitysStorie, function(res) {
+                        expect(res.status).to.equal(200);
+                        done();
+                    });
+            });
+    });
 
-  //return list of the selected epic's activity.
-  it('GET /projects/{project_id}/epics/{epic_id}/activity', function (done) {    
-    var myActivitysEpic = endPoints.activity.myActivitysEpic.replace('{project_id}', project_id);
-    myActivitysEpic = myActivitysEpic.replace('{epic_id}', chance.integer());
-    generalLib
-      .get(token.api_token, myActivitysEpic, function(res){
-        expect(res.status).to.equal(404);
-        done();
-      });    
-  });
-});//describe
+    //return list of the selected epic's activity.
+    it('GET /projects/{project_id}/epics/{epic_id}/activity', function(done) {
+        var myActivitysEpic = endPoints.activity.myActivitysEpic.replace('{project_id}', project_id);
+        myActivitysEpic = myActivitysEpic.replace('{epic_id}', chance.integer());
+        generalLib
+            .get(token.api_token, myActivitysEpic, function(res) {
+                expect(res.status).to.equal(404);
+                done();
+            });
+    });
+}); //describe
