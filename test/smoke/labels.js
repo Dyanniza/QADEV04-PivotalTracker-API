9459d572290962f57e53f307de68916d93a2799b
ceciliachalar <ceci.chiqui@gmail.com>
2015-10-22 20:22:28 -0400

    smoke testing of labels according generalLib

--- test/smoke/labels.js
+++ test/smoke/labels.js
@@ -1,173 +1,230 @@
 /*
 @author Cecilia Chalar
-@class SMOKE Tests for Services of Labels.
+@class LABELs for Test Cases the Services of Labels using General library.
  */
-
 var expect = require('chai').expect;
 var request = require('superagent');
 require('superagent-proxy')(request);
-var label = require('../../lib/labelsAPI');
+
+var endPoints = require('..\\..\\endPoints.json');
+var config = require ('..\\..\\config.json');
+var httpMethod = require ('../../lib/generalLib.js');
+var tokenAPI = require ('../../lib/tokenAPI');
+ 
+
+var labelsProjectEndPoint = endPoints.labels.labelsProjectEndPoint;
+var labelsOfProjectEndPoint = endPoints.labels.labelsOfProjectEndPoint;
+var labelsByStoryIdEndPoint = endPoints.labels.labelsByStoryIdEndPoint;
+var storiesEndPoint = endPoints.label.storiesEndPoint;
+var labelByIdProjectIdEndPoint = endPoints.label.labelByIdProjectIdEndPoint;
+var labelIdStoryIdProjectIdEndPoint = endPoints.label.labelIdStoryIdProjectIdEndPoint;
+var projectsEndPoint = endPoints.projects.projectsEndPoint;
+
 
 var projectId = '1447838';
 var storyID = '106308134';
 var labelId = '13076852';
-var story_id = -1;
+
+var userCredentials = config.userCredential;
+var token = null;
 
 
 describe('Smoke test for labels',function(){
 	this.timeout(10000);
 
-	describe('Service projects by project_id /projects/{project_id}/labels',function(){
+	 before('Get Token', function (done) {
+        tokenAPI
+            .getToken(userCredentials, function (res) {
+                expect(res.status).to.equal(200);
+                token = res.body.api_token;                
+                done();                
+            });
+    });   
 
-		it('GET /projects/{project_id}/labels',function(done){
+	before('Create a Project', function (done) {
+		var endPoint = projectsEndPoint;
+		var al = Math.random().toString(36).substring(2);
+		var prjId = al.substr(0,3);
+		var content = {
+					name : 'ProjectToTestLabels'+prjId										
+				};		
 
-			var project_id = -1;
-			request
-				.get('https://www.pivotaltracker.com/services/v5/projects/1447838/labels?date_format=millis')
-				.proxy('http://172.20.240.5:8080')
-				.set('X-TrackerToken','d302f357ddd835b319b50bb8c2ea4e69')
-			.end(function(err,res){
-				expect(res.status).to.equal(200);
-				done();
+		httpMethod				
+				.post(content,token,endPoint,function (res){
+					expect(res.status).to.equal(200);
+					projectId = res.body.id;					
+					done();
 				});
+	});
+	
 
+	describe('Suite of tests for endpoind: /projects/{project_id}/labels',function(){
+
+		var endPoint = labelsProjectEndPoint.replace('{project_id}',projectId);
+		it('GET /projects/{project_id}/labels',function(done){
+						
+			httpMethod			
+				
+				.get(token,endPoint,function(res){
+					expect(res.status).to.equal(200);
+					done();
+				});			
 		});
-
+		
 		it('POST /projects/{project_id}/labels',function(done){
-			var al = Math.random().toString(36).substr(2);
+			var al = Math.random().toString(36).substring(2);
 			var labelName = al.substr(0,4);
 			var content = {
 					name : labelName											
 				};
-			label
-				.postLabel(projectId,content,function(res){
+			httpMethod
+				
+				.post(content,token,endPoint,function(res){
 					expect(res.status).to.equal(200);
 					done();
-
-				});			
+				});
 
 		});
+	
 	});
 
+	
+	describe('Test suite for endPoint: /projects/{project_id}/labels/{label_id}',function(){
 
-	describe('Labels services, endPoint: /projects/{project_id}/labels/{label_id}',function(){
+		var endPoint = labelByIdProjectIdEndPoint.replace('{project_id}',projectId).replace('{label_id}',labelId);
 
-		it('GET /projects/{project_id}/labels/{label_id}',function(done){
-			request
-				.get('https://www.pivotaltracker.com/services/v5/projects/1447838/labels/13078902')
-				.proxy('http://172.20.240.5:8080')
-				.set('X-TrackerToken','d302f357ddd835b319b50bb8c2ea4e69')
-			.end(function(err,res){
-				expect(res.status).to.equal(200);
-				done();
-			});
+		it.skip('GET /projects/{project_id}/labels/{label_id}',function(done){
+
+			httpMethod			
+				
+				.get(token,endPoint,function(res){
+					expect(res.status).to.equal(200);
+					done();
+				});	
 
 		});
 
-		it('PUT /projects/{project_id}/labels/{label_id}',function(done){
+		it.skip('PUT /projects/{project_id}/labels/{label_id}',function(done){
 			var al = Math.random().toString(36).substr(2);
 			var newLabelName = al.substr(0,4);
 			var content = {
 					name : newLabelName											
 				};
 
-			label
-				.putLabel(projectId,labelId, content,function(res){
+			httpMethod
+				
+				.put(content,token,endPoint,function(res){
 					expect(res.status).to.equal(200);
 					done();
-
-				});				
+				});					
 		});
 
 		it.skip('DELETE /projects/{project_id}/labels/{label_id}',function(done){
-			storyID = -1;
-			label
-				.deleteLabel(projectId,storyID,labelId, content,function(res){
-					expect(res.status).to.equal(200);
+			httpMethod
+				
+				.del(token,endPoint,function(res){
+					expect(res.status).to.equal(204);
 					done();
-
-				});
+				});	
 
 		});
 	});
+	
+	describe ('test suite endPoint: /projects/{project_id}/stories/{story_id}/labels',function(){
 
-	describe ('Labels services,endPoint: /projects/{project_id}/stories/{story_id}/labels',function(){
+
+		beforeEach('Create stories in the project',function(done){	
+			var eP = storiesEndPoint.replace('{project_id}',projectId);		
+			var content = {
+					name : 'Story to test a label'															
+			};
+
+			httpMethod				
+				.post(content,token,eP,function(res){
+					expect(res.status).to.equal(200);
+					storyID = res.body.id;
+					done();
+				});
+		});
+
+		//var endPoint = labelsProjectEndPoint.replace('{project_id}',projectId);
 
 		it('GET /projects/{project_id}/stories/{story_id}/labels',function(done){
-			request
-				.get('https://www.pivotaltracker.com/services/v5/projects/1447838/stories/105799946/labels')
-				.proxy('http://172.20.240.5:8080')
-				.set('X-TrackerToken','d302f357ddd835b319b50bb8c2ea4e69')
-			.end(function(err,res){
-				expect(res.status).to.equal(200);
-				done();
-			});
+			var endPoint = labelsProjectEndPoint.replace('{project_id}',projectId);
+			httpMethod			
+				
+				.get(token,endPoint,function(res){
+					expect(res.status).to.equal(200);
+					done();
+				});	
 
 		});
 
-		it.skip('POST /projects/{project_id}/stories/{story_id}/labels',function(done){
+		it('POST /projects/{project_id}/stories/{story_id}/labels',function(done){
+			var endPoint = labelsProjectEndPoint.replace('{project_id}',projectId);
+			var al = Math.random().toString(36).substr(2);
+			var labelName = al.substr(0,4);
 			var content = {
-
-					name : 'post3'
-																
+					name : labelName											
 				};
 
-			request
-				.post('https://www.pivotaltracker.com/services/v5/projects/1447838/stories/105799946/labels')
-				.proxy('http://172.20.240.5:8080')
-				.send(content)
-				.set('X-TrackerToken','d302f357ddd835b319b50bb8c2ea4e69')
-				.type('application/json')
-
-			.end(function(err,res){
-				expect(res.status).to.equal(200);
-				done();
-			});
+			httpMethod
+				
+				.post(content,token,endPoint,function(res){
+					expect(res.status).to.equal(200);
+					done();
+				});
 
 		});
 
 
 	});
-
-	describe ('Labels services,endPoint: /projects/{project_id}/stories/{story_id}/labels/{label_id}',function(){
+	
+	describe ('test suite endPoint: /projects/{project_id}/stories/{story_id}/labels/{label_id}',function(){
+		var endPoint = labelIdStoryIdProjectIdEndPoint.replace('{project_id}',projectId).replace('{story_id}',storyID).replace('{label_id}',labelId);
 
 		it.skip('DELETE /projects/{project_id}/stories/{story_id}/labels/{label_id}',function(done){
-			label
-				.deleteLabel(projectId,storyID,labelId, content,function(res){
-					expect(res.status).to.equal(200);
+			
+			httpMethod
+				
+				.del(token,endPoint,function(res){
+					expect(res.status).to.equal(204);
 					done();
+				});	
 
-				});
+		});				
 		
 	});
 
 
+
 	describe ('Labels services,endPoint: /projects/{project_id}/stories}',function(){
+		var endPoint = storiesEndPoint.replace('{project_id}',projectId);
 
 		it('POST /projects/{project_id}/stories',function(done){
-				var content = {
 
-						name : 'labeltest2',
-						labels:['labelREST']											
-					};
+			var al = Math.random().toString(36).substring(2);
+			var labelName = al.substr(0,4);
+			var content = {
+					name : 'Story to test a label',
+					labels : ['labelName']											
+				};
 
-				request
-					.post('https://www.pivotaltracker.com/services/v5/projects/1447838/stories')
-					.proxy('http://172.20.240.5:8080')
-					.send(content)
-					.set('X-TrackerToken','d302f357ddd835b319b50bb8c2ea4e69')
-					.type('application/json')
-
-				.end(function(err,res){
+			httpMethod
+				
+				.post(content,token,endPoint,function(res){
 					expect(res.status).to.equal(200);
 					done();
 				});
 
-			});
-
 		});	
 
-	});
+	});	
 	
 });
+
+
+
+
+
 
