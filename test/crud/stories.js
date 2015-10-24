cd2d45009e218e42e3b0bcedb7ef14f928c06280
Jhasmany Quiroz <Jhasmany.Quiroz@fundacion-jala.org>
2015-10-24 09:38:08 -0400

    CRUD of Stories.js file finished

--- test/crud/stories.js
+++ test/crud/stories.js
@@ -35,11 +35,28 @@ describe('CRUD Test for Stories Service Pivotal Tracker', function(){
             .getToken(userCredential, function (res) {
                 expect(res.status).to.equal(status.ok);
                 token = res.body.api_token;
-                done();                
+                var newProject = {
+                    name: chance.string()
+                };
+                var projectsEndPoint = endPoints.projects.projectsEndPoint;
+                generalLib
+                    .post(newProject, token.api_token, projectsEndPoint, function(res) {
+                        expect(res.status).to.equal(config.status.ok);
+                        newProject.project_id = res.body.id;
+                        done();
+                    });                                
             });
     });
 
-    describe('HTTP METHODS for Stories Service', function(){
+    describe('HTTP METHODS for Stories Service', function (){
+        it('POST /projects/{project_id}/stories', function (done) {
+            storiesEndPoint = storiesEndPoint.replace('{project_id}', newProject.project_id);
+            stories
+                .post(argument, token, storiesEndPoint, function (res){
+                    expect(res.status).to.equal(status.ok);
+                    
+                });
+        });
     	it('GET /projects/{project_id}/stories', function (done) {
     		storiesEndPoint = storiesEndPoint.replace('{project_id}', crudConfig.get);
     		stories
